d395c764c8846c57ee2d85d21e2245b1005288e8
calebkech <calebkemboi61@gmail.com>
2025-01-24 18:49:26 +0300

    new route for customer contact

--- cash_flow/views/customer_contact_view.py
+++ cash_flow/views/customer_contact_view.py
@@ -3,3 +3,10 @@ from sqlalchemy.exc import SQLAlchemyError
 from extensions import db
 from cash_flow.models import CustomerContact
 from .blueprint import cash_flow
+
+@cash_flow.route('/customer_contact', methods=['POST'])
+def create_customer_contact():
+    try:
+        pass
+    except Exception as e:
+        pass
