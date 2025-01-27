81fab88dbe91b0f82a2d5c1ea8ed50b69e190eb1
calebkech <calebkemboi61@gmail.com>
2025-01-27 19:15:49 +0300

    create customer contact route complete, waiting for testing

--- cash_flow/models/customer_contacts.py
+++ cash_flow/models/customer_contacts.py
@@ -5,7 +5,6 @@ from extensions import db
 class CustomerContact(BaseModel):
     __tablename__ = 'customer_contacts'
 
-    id = db.Column(db.Integer, primary_key=True)
     customer_id = db.Column(db.String(36), db.ForeignKey('customers.id'), nullable=False)
     contact_type = db.Column(String(50), nullable=False)  # e.g., 'email', 'phone'
     contact_value = db.Column(String(255), nullable=False)  # e.g., 'example@domain.com', '123-456-7890'
--- cash_flow/views/customer_contact_view.py
+++ cash_flow/views/customer_contact_view.py
@@ -7,9 +7,41 @@ from .blueprint import cash_flow
 @cash_flow.route('/customer_contact', methods=['POST'])
 def create_customer_contact():
     try:
-        pass
+        # Get data from the request
+        data = request.get_json(silent=True)
+        if data is None:
+            return jsonify({"Error": "Invalid JSON in Request Body"}), 400
+    
+        # Validate required fields
+        required_fields = ['contact_type', 'contact_value']
+        missing_fields = [field for field in required_fields if field not in data]
+        if missing_fields:
+            return jsonify({"error": f'Missing Required field(s): {", ".join(missing_fields)}'}), 400
+        
+        contact_type = data.get('contact_type')
+        contact_value = data.get('contatc_value')
+
+        existing_details = CustomerContact.query.filter(CustomerContact.contact_value == contact_value)
+        if existing_details:
+            return jsonify({"Error": "Contact Value Already Exist"}), 400
+        
+        # Create New Customer Contact
+        new_customer_contact = CustomerContact(
+            contact_type = contact_type,
+            contact_value = contact_value
+        )
+
+        # Save Using the Basemodel Method
+        new_customer_contact.save(db.session)
+        return jsonify({"Message": "Contact saved Successfully"}), 200
+    
+    # just incase... reduce debug time
+    except SQLAlchemyError as e:
+        db.session.rollback()
+        return jsonify({"error": "Database error Occurred", "Details": str(e)}), 500
+    
     except Exception as e:
-        pass
+        return jsonify({"error": "An Unexpected error Occurred", "Details": str(e)}), 500
 
 @cash_flow.route('/customer_contact/<string:customer_contact_id>', methods=['GET'])
 def get_customer_contacts(customer_contact_id):
