825a8f0d086e01b8e9a5bdb0744058657c513f85
calebkech <calebkemboi61@gmail.com>
2025-01-29 16:33:40 +0300

    create all customer routes, waiting to be tested

--- cash_flow/views/customer_contact_view.py
+++ cash_flow/views/customer_contact_view.py
@@ -3,6 +3,7 @@ from sqlalchemy.exc import SQLAlchemyError
 from extensions import db
 from cash_flow.models import CustomerContact
 from .blueprint import cash_flow
+import uuid
 
 @cash_flow.route('/customer_contact', methods=['POST'])
 def create_customer_contact():
@@ -45,10 +46,23 @@ def create_customer_contact():
 
 @cash_flow.route('/customer_contact/<string:customer_contact_id>', methods=['GET'])
 def get_customer_contacts(customer_contact_id):
+    """
+    Fetch all the customer contacts except the soft deleted
+    """
     try:
-        pass
+        # Filter contact where deleted_at is None
+        customer_contacts = CustomerContact.query.filter(CustomerContact.deleted_at.is_(None)).all()
+
+        #serialize customers
+        customer_contact_list = [customer_contacts.to_dict() for customer_contact in customer_contacts]
+        return jsonify(customer_contact_list), 200
+    
     except Exception as e:
-        pass
+        return jsonify({
+            "Error": "An Unxpected error Occurred",
+            "details": str(e)
+        }), 500
+    
 
 @cash_flow.route('/cutomer_contact/<string:customer_contact_id>', methods=['PUT'])
 def update_customer_contact(customer_contact_id):
@@ -87,20 +101,72 @@ def update_customer_contact(customer_contact_id):
 
 @cash_flow.route('/customer_contact/<string:customer_contact_id>', methods=['DELETE'])
 def soft_delete_customer_contact(customer_contact_id):
+    """
+    Soft delete a customer contact by its Id
+    """
     try:
-        pass
+        # fetct the conatact using active querry
+        customer_contact = CustomerContact.active_query().filter_by(id = customer_contact_id).first()
+        if not customer_contact:
+            return jsonify({"error": "Contact not found"}), 404
+        
+        # soft delete the contact if customer_contact is not None
+        customer_contact.soft_delete(db.session)
+        return jsonify({"message": "Contact Soft Deleted successfully"}), 200
+    
     except Exception as e:
-        pass
+        return jsonify({
+            "error": "an unexpected error occured",
+            "details": str(e)
+        }), 500
 
 @cash_flow.route('/customer_contact/<string:customer_contact_id>', methods=['POST'])
 def restore_customer_contact(customer_contact_id):
+    """
+    Restore soft deleted contact by its Id
+    """
     try:
-        pass
+        # Fetch soft deleted conatact 
+        customer_contact = CustomerContact.query.filter(CustomerContact.id == customer_contact_id, CustomerContact.deleted_at.isnot(None)).first()
+
+        if not customer_contact:
+            return jsonify({"error": "Contact not found"}), 404
+        
+        # Restore the contact by setting deleted_at timestamp to Non
+        customer_contact.deleted_at = None
+        db.session.commit()
+
+        return jsonify({"message": "Contact restored successfully"}), 200
+    
     except Exception as e:
-        pass
+        return jsonify({
+            "error": "an unexpected error occured",
+            "details": str(e)
+        }), 500
+
 @cash_flow.route('/customer_contact/<string:customer_contact_id>', methods=['DELETE'])
 def delete_customer_contact(customer_contact_id):
+    """
+    Delete contact by its Id
+    """
     try:
-        pass
+        try:
+
+            # Validate UUID Format
+            uuid_obj = uuid.UUID(customer_contact_id)
+        except ValueError:
+            return jsonify({"error": "Invalid Contact Id"})
+        
+        # Fetch contact to be deleted form db
+        customer_contact = CustomerContact.query.get(customer_contact_id)
+        if not customer_contact:
+            return jsonify({"error": "contact not found"}), 404
+        
+        # Delete contact using basemodel's delete
+        customer_contact.delete(db.session)
+        return jsonify({"error": "contact deleted successfully"}), 200
     except Exception as e:
-        pass
+        return jsonify({
+            "error": "an unexpected error occured",
+            "Details": str(e)
+        }), 500
