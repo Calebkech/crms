175e71cb36546f7e1d331eab25e0d45485f826f5
calebkech <calebkemboi61@gmail.com>
2025-02-02 19:15:54 +0300

    created new route for products and services

---
+++ cash_flow/views/product_service_view.py
@@ -0,0 +1,13 @@
+from flask import jsonify, Blueprint, request
+from cash_flow.models import ProductService
+from .blueprint import cash_flow
+from extensions import db
+from sqlalchemy.exc import SQLAlchemyError
+import uuid
+
+@cash_flow.route('/product_service', methodes=['POST'])
+def create_product_service():
+    try:
+        pass
+    except  Exception as e:
+        pass
